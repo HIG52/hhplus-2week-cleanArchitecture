2ec61d6229a4daac3fa62c88663030ce595d02cb
홍준영 <hongjy1997@naver.com>
2024-12-23 01:38:25 +0900

    :tada: init : 프로젝트 초기화

---
+++ src/main/java/com/hjy/lecture/hhplus2weekcleanarchitecture/Hhplus2weekCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.hjy.lecture.hhplus2weekcleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hhplus2weekCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hhplus2weekCleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hjy/lecture/hhplus2weekcleanarchitecture/Hhplus2weekCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjy.lecture.hhplus2weekcleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hhplus2weekCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
