024ac72a2cae897d655386107742cfe5a2486eab
홍준영 <hongjy1997@naver.com>
2024-12-26 01:28:27 +0900

    :sparkles: feat : 동시성 / 40명입력시 30명만 입력되도록 / 한유저가 동시에 입력시 1회만 insert되도록

--- src/main/java/com/hjy/lecture/hhplus2weekcleanarchitecture/application/service/LectureStudentService.java
+++ src/main/java/com/hjy/lecture/hhplus2weekcleanarchitecture/application/service/LectureStudentService.java
@@ -1,12 +1,16 @@
 package com.hjy.lecture.hhplus2weekcleanarchitecture.application.service;
 
+import com.hjy.lecture.hhplus2weekcleanarchitecture.domain.entity.Lecture;
+import com.hjy.lecture.hhplus2weekcleanarchitecture.infrastructure.repository.LectureRepository;
 import com.hjy.lecture.hhplus2weekcleanarchitecture.infrastructure.repository.LectureStudentRepository;
 import com.hjy.lecture.hhplus2weekcleanarchitecture.domain.entity.LectureStudent;
 import com.hjy.lecture.hhplus2weekcleanarchitecture.presentation.dto.LectureResponseDTO;
 import com.hjy.lecture.hhplus2weekcleanarchitecture.presentation.dto.LectureStudentRequestDTO;
 import com.hjy.lecture.hhplus2weekcleanarchitecture.presentation.dto.LectureStudentResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,21 +18,27 @@ import java.util.Optional;
 public class LectureStudentService {
 
     private final LectureStudentRepository lectureStudentRepository;
+    private final LectureRepository lectureRepository;
 
     public static final int MAX_STUDENTS = 30;
 
     @Autowired
-    public LectureStudentService(LectureStudentRepository lectureStudentRepository) {
+    public LectureStudentService(LectureStudentRepository lectureStudentRepository, LectureRepository lectureRepository) {
         this.lectureStudentRepository = lectureStudentRepository;
+        this.lectureRepository = lectureRepository;
     }
 
+    @Transactional
     public LectureStudentResponseDTO applyLecture(LectureStudentRequestDTO lectureStudentRequestDTO) {
         LectureStudent lectureStudent = LectureStudent.createLectureStudent(
                 lectureStudentRequestDTO.getLectureId(),
                 lectureStudentRequestDTO.getUserId()
         );
+        // 비관적 락을 사용하여 강의 데이터를 잠금
+        Lecture lecture = lectureRepository.findByIdWithLock(lectureStudentRequestDTO.getLectureId())
+                .orElseThrow(() -> new IllegalArgumentException("강의를 찾을 수 없습니다."));
 
-        int currentStudentCount = lectureStudentRepository.countByLectureId_LectureId(lectureStudent.getLectureId().getLectureId());
+        int currentStudentCount = lectureStudentRepository.countByLectureId_LectureId(lecture.getLectureId());
 
         if (currentStudentCount >= MAX_STUDENTS) {
             throw new IllegalStateException("해당 강의는 정원이 초과되었습니다.");
@@ -43,7 +53,11 @@ public class LectureStudentService {
             throw new IllegalArgumentException("이미 수강신청한 강의입니다.");
         }
 
-        lectureStudentRepository.save(lectureStudent);
+        try {
+            lectureStudentRepository.save(lectureStudent);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("이미 등록된 수강신청 정보입니다.", e);
+        }
 
         LectureStudentResponseDTO lectureStudentResponseDTO = new LectureStudentResponseDTO();
         lectureStudentResponseDTO.setLectureId(lectureStudent.getLectureId().getLectureId());
